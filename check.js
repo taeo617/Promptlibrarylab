c1c3db1a72a19dc394811e42aa678b78744300a6
taeo617 <kty3974@gmail.com>
2026-06-12 10:26:49 +0900

    오류 수정

--- check.js
+++ check.js
@@ -1,10 +1,20 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-  page.on('pageerror', err => console.log('PAGE ERROR:', err.message));
-  await page.goto('file:///c:/Users/User/Desktop/HOME/Promptlibrarylab/index.html', { waitUntil: 'networkidle0' });
-  await new Promise(r => setTimeout(r, 1000));
-  await browser.close();
-})();
+const fs=require('fs');
+const appJs=fs.readFileSync('app.js','utf8');
+const indexHtml=fs.readFileSync('index.html','utf8');
+
+const vars = ['canvas','navLoginBtn','navUserBadge','dropdownMypageBtn','dropdownLogoutBtn',
+'loginModalBackdrop','loginModalClose','loginSubmitBtn','loginPwInput','loginIdInput',
+'profileModalBackdrop','profileModalClose','profileBtnAdd','profileBtnDelete','profileInput',
+'authorInput','lightboxClose','libModalBackdrop','libModalClose','libModalCopy','libModalEdit',
+'libModalCancel','libModalSave','libModalEditTextareaEn','mobileMenuOverlay','navFloat',
+'navLibrary','filterNewest','filterOldest','textarea','submitBtn','toggleListBtn'];
+
+vars.forEach(v=>{
+    const m = appJs.match(new RegExp('(?:const|let|var)\\s+'+v+'\\s*=\\s*document\\.getElementById\\([\\\'\\"](.*?)[\\\'\\"]\\)'));
+    if(m) {
+        const id=m[1];
+        if(!indexHtml.includes('id=\"'+id+'\"') && !indexHtml.includes("id='"+id+"'")) {
+            console.log('Missing in index.html: ' + id + ' (var: ' + v + ')');
+        }
+    }
+});
